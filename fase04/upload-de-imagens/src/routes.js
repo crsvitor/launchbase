86861aa79010274cdbce5b059d2e78602129d81b
Vitor de Castro <v.crs@hotmail.com>
2020-08-11 05:28:50 -0300

    Today's update

--- fase04/upload-de-imagens/src/routes.js
+++ fase04/upload-de-imagens/src/routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const routes = express.Router();
 const multer = require('./app/middlewares/multer');
 const ProductController = require('./app/controllers/ProductController');
+const Product = require('./app/models/Product');
 
 routes.get("/", function (req, res) {
     return res.render('layout.njk');
 });
 
 routes.get('/products/create', ProductController.create);
+routes.get('/products/:id', ProductController.show);
 routes.get('/products/:id/edit', ProductController.edit);
 
 routes.post('/products', multer.array("photos", 6), ProductController.post);
