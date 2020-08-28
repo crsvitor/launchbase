ecb1b21e8c73db57bd4891f2844e19f51471e27d
Vitor de Castro <v.crs@hotmail.com>
2020-08-28 00:59:28 -0300

    Updatings

--- fase04/controle-da-sessao-de-usuarios/src/app/controllers/UserController.js
+++ fase04/controle-da-sessao-de-usuarios/src/app/controllers/UserController.js
@@ -6,6 +6,8 @@ module.exports = {
         return res.render("user/register");
     },
     async show(req, res) {
+        const { user } = req;
+
         user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj);
         user.cep = formatCep(user.cep);
         
@@ -18,5 +20,33 @@ module.exports = {
         req.session.userId = userId;
 
         return res.redirect('/users');
+    },
+    async update(req, res) {
+         try {
+            const { user } = req;
+            let { name, email, cpf_cnpj, cep, address } = req.body;
+
+            cpf_cnpj = cpf_cnpj.replace(/\D/g, "");
+            cep = cep.replace(/\D/g, "");
+
+            await User.update(user.id, {
+                name,
+                email,
+                cpf_cnpj,
+                cep,
+                address
+            });
+
+            return res.render("user/index", {
+                user: req.body,
+                success: "Conta atualizada com sucesso!"
+            });
+
+         } catch {
+                console.error(err)
+                return res.render("user/index", {
+                    error: "Algum erro ocorreu!"
+                });
+         }
     }
 }
